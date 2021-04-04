0e561b8715a5e4e899da92ae4db7f219ced7c549
Jeff Gichuki <julian.gichuki@gmail.com>
2021-04-04 21:30:50 +0300

    Merge Trigger script added.
    Added a script that controls the triggering of the merge functionality.

---
+++ MergeTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MergeTrigger : MonoBehaviour
+{
+    private BoidsController[] AI;
+    private AISeparator[] separators;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        switch (other.CompareTag("Player"))
+        {
+            case true:
+                AI = FindObjectsOfType<BoidsController>();
+                separators = FindObjectsOfType<AISeparator>();
+                foreach(BoidsController ai in AI)
+                {
+                    foreach (AISeparator sep in separators)
+                    {
+                        sep.m_spaceBetween = 0;
+                    }
+                    ai.m_spaceBetween = 0;
+                    ai.Invoke("Merge", 2f);
+                }
+                break;
+            case false:
+                break;
+        }
+    }
+}
