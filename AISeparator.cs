fd6dfbcd5b78e59c9fbdd5a477203cecc88b9b9e
Jeff Gichuki <julian.gichuki@gmail.com>
2021-04-04 16:39:06 +0300

    Created AI Separator
    Controls the distancing of the Boids AI from each other.

---
+++ AISeparator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AISeparator : MonoBehaviour
+{
+    private GameObject[] AI;
+
+    [SerializeField] private float m_spaceBetween = 1.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AI = GameObject.FindGameObjectsWithTag("AI");    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(GameObject ai in AI)
+        {
+            switch(ai != gameObject)
+            {
+                case true:
+                    float distance = Vector3.Distance(ai.transform.position, this.transform.position);
+                    switch (distance <= m_spaceBetween)
+                    {
+                        case true:
+                            Vector3 direction = transform.position - ai.transform.position;
+                            transform.localPosition += direction * Time.deltaTime;
+                            break;
+                        case false:
+                            break;
+                    }
+                    break;
+                case false:
+                    break;
+            }
+        }
+    }
+}
