a85343bb044c3a00f5884038e0b63d2b9d67ab13
Jeff Gichuki <julian.gichuki@gmail.com>
2021-04-04 16:38:34 +0300

    Created Boids Trigger zones.
    Controls the AI follow functionality when the player enters the trigger zones.

---
+++ BoidsTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoidsTrigger : MonoBehaviour
+{
+    [SerializeField] private BoidsController m_AI;
+
+    [SerializeField] private bool b_canFollow;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        switch (other.CompareTag("Player")) {
+            case true:
+                m_AI.m_ShouldFollow = b_canFollow;
+                break;
+            case false:
+                break;
+        }
+    }
+}
