67285a2d0cbc986da06377e5ca75d2b3ce32c319
Jeff Gichuki <julian.gichuki@gmail.com>
2021-04-05 22:03:11 +0300

    Added Game Manager
    Controls the Flow of events in the whole game.

---
+++ GameManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private Transform m_player;
+
+    [SerializeField] private Animator m_playerAnimator;
+    
+
+    public float maxSize;
+    public float growFactor;
+    public float waitTime;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Merge()
+    {
+        m_playerAnimator.SetTrigger("Grow");
+        StartCoroutine(Scale());
+    }
+
+    IEnumerator Scale()
+    {
+        float timer = 0;
+
+
+        while (true) // this could also be a condition indicating "alive or dead"
+        {
+            // we scale all axis, so they will have the same value, 
+            // so we can work with a float instead of comparing vectors
+            while (maxSize > m_player.transform.localScale.x)
+            {
+                timer += Time.deltaTime;
+                m_player.transform.localScale += new Vector3(1, 1, 1) * Time.deltaTime * growFactor;
+                yield return null;
+            }
+            // reset the timer
+            
+
+            yield return new WaitForSeconds(waitTime);
+
+            timer = 0;
+            while (1 < transform.localScale.x)
+            {
+                timer += Time.deltaTime;
+                transform.localScale -= new Vector3(1, 1, 1) * Time.deltaTime * growFactor;
+                yield return null;
+            }
+
+            timer = 0;
+            yield return new WaitForSeconds(waitTime);
+        }
+
+    }
+}
