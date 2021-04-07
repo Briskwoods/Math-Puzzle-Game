f631daee04185a547b1b2c2e3dba7780851cf694
Jeff Gichuki <julian.gichuki@gmail.com>
2021-04-07 20:44:00 +0300

    Created Math Trigger
    On player entry, it checks the player direction and performs and add or subtract function depending on the direction the player is moving.

---
+++ MathsTrigger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MathsTrigger : MonoBehaviour
+{
+
+    [SerializeField] private GameManager m_gameManger;
+    [SerializeField] private TouchController m_player;
+
+    [SerializeField] private int m_value;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        switch (other.CompareTag("Player") && m_player.m_movingForward)
+        {
+            case true:
+                m_gameManger.Add(m_value);
+                break;
+            case false:
+                break;
+        }
+
+        switch (other.CompareTag("Player") && m_player.m_movingBack)
+        {
+            case true:
+                m_gameManger.Subtract(m_value);
+                break;
+            case false:
+                break;
+        }
+    }
+}
