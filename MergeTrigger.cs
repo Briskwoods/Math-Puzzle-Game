aa04f6a74f3c410b333d05c958ae870fd609f296
Jeff Gichuki <julian.gichuki@gmail.com>
2021-04-12 20:24:38 +0300

    Update MergeTrigger.cs
    Updated Functionality:
    The Merge trigger also detects when to trigger the enemy moving to the player.

--- MergeTrigger.cs
+++ MergeTrigger.cs
@@ -6,17 +6,16 @@ public class MergeTrigger : MonoBehaviour
 {
     [SerializeField] private GameManager m_gameManager;
 
-    private BoidsController[] AI;
-    private AISeparator[] separators;
-    
+    [SerializeField] private BossController m_bossController;
 
+    private BoidsController[] AI;
+    
     private void OnTriggerEnter(Collider other)
     {
         switch (other.CompareTag("Player"))
         {
             case true:
                 AI = FindObjectsOfType<BoidsController>();
-                separators = FindObjectsOfType<AISeparator>();
                 foreach(BoidsController ai in AI)
                 {
                     ai.m_spaceBetween = 0;
@@ -33,6 +32,26 @@ public class MergeTrigger : MonoBehaviour
                 foreach (BoidsController ai in AI)
                 {
                     ai.m_mergeZone = true;
+                }
+                break;
+            case false:
+                break;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        switch (other.CompareTag("Player"))
+        {
+            case true:
+                AI = FindObjectsOfType<BoidsController>();
+                switch (AI.Length == 0 && !m_bossController.AlreadyCalled)
+                {
+                    case true:
+                        m_bossController.TriggerEnemyMoveToPlayer();
+                        break;
+                    case false:
+                        break;
                 }
                 break;
             case false:
