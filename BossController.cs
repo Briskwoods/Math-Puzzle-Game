eda5d3ecb4e70ea10b8583ffd9c35ecafa482f94
Jeff Gichuki <julian.gichuki@gmail.com>
2021-04-12 20:22:31 +0300

    Created Boss Controller
    Triggers the boss to move to the player and start a fight.

---
+++ BossController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class BossController : MonoBehaviour
+{
+    [SerializeField] private Animator m_animator;
+
+    [SerializeField] private GameManager m_gameManager;
+
+    [SerializeField] private Transform m_fightZone;
+
+
+    public bool AlreadyCalled = false;
+
+    public void TriggerEnemyMoveToPlayer() 
+    {
+        AlreadyCalled = true;
+        m_animator.SetTrigger("Run");
+        transform.DOMove(m_fightZone.position, 1f).OnComplete(StartFightNow);
+    }
+
+    public void StartFightNow() 
+    {
+        m_gameManager.TriggerFight();
+    }
+}
