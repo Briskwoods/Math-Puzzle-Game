a62b805a5f4e223e227e4d1fc9121ef3db336ab1
Jeff Gichuki <julian.gichuki@gmail.com>
2021-04-19 22:47:10 +0300

    Update MathsTrigger.cs
    Fixed bug where the math triggers would add more values than expected as a result of satisfying multiple conditions.

--- MathsTrigger.cs
+++ MathsTrigger.cs
@@ -12,22 +12,79 @@ public class MathsTrigger : MonoBehaviour
     
     private void OnTriggerEnter(Collider other)
     {
-        switch (other.CompareTag("Player") && m_player.m_movingForward)
+        Debug.Log("Called "+other.gameObject.name +" - "+ other.CompareTag("Player"));
+
+
+        switch (other.CompareTag("Player") && m_player.m_movingForward && !m_player.m_movingLeft && !m_player.m_movingRight)
         {
             case true:
                 m_gameManger.Add(m_value);
+                m_player.m_movingForward = false;
                 break;
             case false:
                 break;
+
+                return;
         }
 
-        switch (other.CompareTag("Player") && m_player.m_movingBack)
+        switch (other.CompareTag("Player") && m_player.m_movingBack && !m_player.m_movingLeft && !m_player.m_movingRight)
         {
             case true:
                 m_gameManger.Subtract(m_value);
+                m_player.m_movingBack = false;
                 break;
             case false:
                 break;
+
+                return;
+        }
+
+        switch (other.CompareTag("Player") && m_player.isCentre && m_player.m_movingRight && !m_player.m_movingBack && !m_player.m_movingForward)
+        {
+            case true:
+                m_gameManger.Add(m_value);
+                m_player.m_movingRight = false;
+                break;
+            case false:
+                break;
+
+                return;
+        }
+
+        switch (other.CompareTag("Player") && m_player.isCentre && m_player.m_movingLeft && !m_player.m_movingBack && !m_player.m_movingForward)
+        {
+            case true:
+                m_gameManger.Add(m_value);
+                m_player.m_movingLeft = false;
+                break;
+            case false:
+                break;
+
+                return;
+        }
+
+        switch (other.CompareTag("Player") && m_player.isLeft && m_player.m_movingRight)
+        {
+            case true:
+                m_gameManger.Subtract(m_value);
+                m_player.m_movingRight = false;
+                break;
+            case false:
+                break;
+
+                return;
+        }
+
+        switch (other.CompareTag("Player") && m_player.isRight && m_player.m_movingLeft)
+        {
+            case true:
+                m_gameManger.Subtract(m_value);
+                m_player.m_movingLeft = false;
+                break;
+            case false:
+                break;
+
+                return;
         }
     }
 }
