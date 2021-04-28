7f84e047941fff3b0713854f24aa44349524e39b
Jeff Gichuki <julian.gichuki@gmail.com>
2021-04-28 21:29:00 +0300

    Added Horizontal Movement trigger.
    Sets the direction of motion the player moves in to control horizontal math functionality.

---
+++ HorizontalMovementTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalMovementTrigger : MonoBehaviour
+{
+    [SerializeField] private TouchController m_player;
+
+    [SerializeField] private bool b_isFromLeft;
+    [SerializeField] private bool b_isFromRight;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        switch (other.CompareTag("Player"))
+        {
+            case true:
+                m_player.isFromLeft = b_isFromLeft;
+                m_player.isFromRight = b_isFromRight;
+                break;
+            case false:
+                break;
+        }
+    }
+}
