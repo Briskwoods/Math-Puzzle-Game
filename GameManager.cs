c2072439b026833d6dcb031fd5a2168f7545798f
Jeff Gichuki <julian.gichuki@gmail.com>
2021-05-01 00:07:46 +0300

    Update GameManager.cs
    Added Vibrations to the fight.
    Set the game to save the level number on start, used for loading level when player quits.
    Also controls the position of UI elements above the player and boss.

--- GameManager.cs
+++ GameManager.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     
-    [SerializeField] private TextMeshProUGUI m_Score;
+    [SerializeField] private TextMeshProUGUI m_scoreText;
 
     [SerializeField] private MenuController m_menu;
 
@@ -18,6 +19,13 @@ public class GameManager : MonoBehaviour
     [SerializeField] private Animator m_bossAnimator;
 
     [SerializeField] private int m_TargetTotal;
+
+    [SerializeField] private GameObject m_playerPlaceholder;
+    [SerializeField] private GameObject m_bossPlaceholder;
+
+    [SerializeField] private GameObject m_score;
+    [SerializeField] private GameObject m_target;
+
 
     public int m_currentTotal = 0;
 
@@ -32,19 +40,23 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        Vibration.Init();
+        PlayerPrefs.SetInt("Level", SceneManager.GetActiveScene().buildIndex);
     }
 
     // Update is called once per frame
     void Update()
     {
-        m_Score.text = m_currentTotal + "";
+        m_scoreText.text = m_currentTotal + "";
+        m_score.transform.position = m_playerPlaceholder.transform.position;
+        m_target.transform.position = m_bossPlaceholder.transform.position;
+
     }
 
     public void Merge()
     {
         m_playerAnimator.SetTrigger("Grow");
-        StartCoroutine(Scale());      
+        StartCoroutine(Scale());
     }
 
     public void Add(int numberToAdd)
@@ -69,6 +81,7 @@ public class GameManager : MonoBehaviour
         m_bossAnimator.SetBool("Lose", true);
 
         m_menu.m_winMenu.SetActive(true);
+        Vibration.Vibrate(100);
     }
 
 
@@ -80,17 +93,20 @@ public class GameManager : MonoBehaviour
         m_playerAnimator.SetTrigger("Lose");
         m_bossAnimator.SetBool("Win", true);
 
-
         m_menu.m_loseMenu.SetActive(true);
+        Vibration.Vibrate(200);
     }
 
     public void TriggerFight()
     {
+        m_score.SetActive(false);
+        m_target.SetActive(false);
         StartCoroutine(Fight());
     }
 
     IEnumerator Scale()
     {
+
         float timer = 0;
 
 
@@ -102,6 +118,7 @@ public class GameManager : MonoBehaviour
             {
                 timer += Time.deltaTime;
                 m_player.transform.localScale += new Vector3(1, 1, 1) * Time.deltaTime * growFactor;
+                Vibration.VibratePeek();
                 yield return null;
             }
             // reset the timer
@@ -114,21 +131,20 @@ public class GameManager : MonoBehaviour
             {
                 timer += Time.deltaTime;
                 transform.localScale -= new Vector3(1, 1, 1) * Time.deltaTime * growFactor;
+                Vibration.VibratePeek();
                 yield return null;
             }
 
             timer = 0;
             yield return new WaitForSeconds(waitTime);
-            //m_player.transform.LookAt(m_boss.position);
-            //m_boss.transform.LookAt(m_player.position);
         }
     }
 
     IEnumerator Fight()
     {
         isFighting = true;
-        
-        switch(m_currentTotal == m_TargetTotal){
+
+        switch (m_currentTotal == m_TargetTotal){
             case true:
                 m_player.transform.LookAt(m_boss.position);
                 m_boss.transform.LookAt(m_player.position);
@@ -137,6 +153,7 @@ public class GameManager : MonoBehaviour
                 m_player.transform.LookAt(m_boss.position);
                 m_boss.transform.LookAt(m_player.position);
                 m_playerAnimator.SetTrigger("Hit");
+                Vibration.VibratePeek();
                 yield return new WaitForSeconds(1.2f);
                 m_player.transform.LookAt(m_boss.position);
                 m_boss.transform.LookAt(m_player.position);
@@ -145,6 +162,7 @@ public class GameManager : MonoBehaviour
                 m_player.transform.LookAt(m_boss.position);
                 m_boss.transform.LookAt(m_player.position);
                 m_bossAnimator.SetTrigger("Hit");
+                Vibration.VibratePeek();
                 yield return new WaitForSeconds(1f);
                 m_player.transform.LookAt(m_boss.position);
                 m_boss.transform.LookAt(m_player.position);
@@ -153,6 +171,7 @@ public class GameManager : MonoBehaviour
                 m_player.transform.LookAt(m_boss.position);
                 m_boss.transform.LookAt(m_player.position);
                 m_playerAnimator.SetTrigger("Hit");
+                Vibration.VibratePeek();
                 yield return new WaitForSeconds(1.2f);
                 m_player.transform.LookAt(m_boss.position);
                 m_boss.transform.LookAt(m_player.position);
@@ -161,6 +180,7 @@ public class GameManager : MonoBehaviour
                 m_player.transform.LookAt(m_boss.position);
                 m_boss.transform.LookAt(m_player.position);
                 m_bossAnimator.SetTrigger("Hit");
+                Vibration.VibratePeek();
                 yield return new WaitForSeconds(1.2f);
                 m_player.transform.LookAt(m_boss.position);
                 m_boss.transform.LookAt(m_player.position);
@@ -181,6 +201,7 @@ public class GameManager : MonoBehaviour
                 m_player.transform.LookAt(m_boss.position);
                 m_boss.transform.LookAt(m_player.position);
                 m_bossAnimator.SetTrigger("Hit");
+                Vibration.VibratePeek();
                 yield return new WaitForSeconds(1f);
                 m_player.transform.LookAt(m_boss.position);
                 m_boss.transform.LookAt(m_player.position);
@@ -189,6 +210,7 @@ public class GameManager : MonoBehaviour
                 m_player.transform.LookAt(m_boss.position);
                 m_boss.transform.LookAt(m_player.position);
                 m_playerAnimator.SetTrigger("Hit");
+                Vibration.VibratePeek();
                 yield return new WaitForSeconds(1f);
                 m_player.transform.LookAt(m_boss.position);
                 m_boss.transform.LookAt(m_player.position);
@@ -197,6 +219,7 @@ public class GameManager : MonoBehaviour
                 m_player.transform.LookAt(m_boss.position);
                 m_boss.transform.LookAt(m_player.position);
                 m_bossAnimator.SetTrigger("Hit");
+                Vibration.VibratePeek();
                 yield return new WaitForSeconds(1f);
                 m_player.transform.LookAt(m_boss.position);
                 m_boss.transform.LookAt(m_player.position);
@@ -205,6 +228,7 @@ public class GameManager : MonoBehaviour
                 m_player.transform.LookAt(m_boss.position);
                 m_boss.transform.LookAt(m_player.position);
                 m_playerAnimator.SetTrigger("Hit");
+                Vibration.VibratePeek();
                 yield return new WaitForSeconds(1f);
                 m_player.transform.LookAt(m_boss.position);
                 m_boss.transform.LookAt(m_player.position);
@@ -220,5 +244,9 @@ public class GameManager : MonoBehaviour
                 break;
         }
 
+    }
+    public int Getint(string KeyName)
+    {
+        return PlayerPrefs.GetInt(KeyName);
     }
 }
