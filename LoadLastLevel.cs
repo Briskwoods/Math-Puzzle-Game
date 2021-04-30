7c7af640ea95377b5b90b4dfdbc6b1108084ce9a
Jeff Gichuki <julian.gichuki@gmail.com>
2021-05-01 00:08:37 +0300

    Created Load Last Level
    A script that loads the last level the player was on as they closed the application.

---
+++ LoadLastLevel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLastLevel : MonoBehaviour
+{
+    [SerializeField] private GameManager gameManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        switch (gameManager.Getint("Level")== 0)
+        {
+            case true:
+                SceneManager.LoadScene(1);
+                break;
+            case false:
+                SceneManager.LoadScene(gameManager.Getint("Level"));
+                break;
+        }
+
+    }
+
+
+}
