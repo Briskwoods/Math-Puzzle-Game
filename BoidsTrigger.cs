f5c5d127c1af02fcd2abd3679230e1376072c24d
Jeff Gichuki <julian.gichuki@gmail.com>
2021-05-01 00:05:51 +0300

    Update BoidsTrigger.cs
    Added Vibrations when boids collide with player to merge

--- BoidsTrigger.cs
+++ BoidsTrigger.cs
@@ -10,12 +10,18 @@ public class BoidsTrigger : MonoBehaviour
 
     [SerializeField] private bool b_canFollow;
 
+    private void Start()
+    {
+        Vibration.Init();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         switch (other.CompareTag("Player")) {
             case true:
                 m_AI.m_ShouldFollow = b_canFollow;
                 m_boidsAnimator.SetBool("Run",b_canFollow);
+                Vibration.VibratePeek();
                 break;
             case false:
                 break;
